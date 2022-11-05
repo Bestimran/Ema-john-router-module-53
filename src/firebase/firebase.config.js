eddeab350626f971ab0b15fd6d74782a70f8ba01
Bestimran <imrantahir9918@gmail.com>
2022-11-05 08:46:09 -0700

    router added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA2DndOegebV15rcXpnEBaVcXHwaMdbFSQ",
+  authDomain: "ema-john-router-auth-4f920.firebaseapp.com",
+  projectId: "ema-john-router-auth-4f920",
+  storageBucket: "ema-john-router-auth-4f920.appspot.com",
+  messagingSenderId: "939736983790",
+  appId: "1:939736983790:web:285dfcb222fc3ea8b9abff"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
